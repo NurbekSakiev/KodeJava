e6c83f25c1908a7807eb32a11816842c80064058
NurbekSakiev <sakin.snn@gmail.com>
2014-09-26 11:33:09 -0500

    the UnderscoreNumeric has been modified

--- KodeJava/src/UnderscoreNumeric.java
+++ KodeJava/src/UnderscoreNumeric.java
@@ -6,10 +6,16 @@ public class UnderscoreNumeric {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int maxInt = 2_147_483_647;
-		int x = Integer.parseInt("00101010",2);
-		
-		System.out.print(x);
-		
+        int minInt = -2_147_483_648;
+ 
+        if (maxInt == Integer.MAX_VALUE) {
+            System.out.println("maxInt = " + maxInt);
+        }
+ 
+        if (minInt == Integer.MIN_VALUE) {
+            System.out.println("minInt = " + minInt);
+        }
+ 
 		
 		}
 	}
