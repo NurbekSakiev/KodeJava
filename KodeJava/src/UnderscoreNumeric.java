da6f284bd5afa397e4733458413dbd0e7c533575
NurbekSakiev <sakin.snn@gmail.com>
2014-09-25 23:53:10 -0500

    numeric literals with underscore

---
+++ KodeJava/src/UnderscoreNumeric.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+public class UnderscoreNumeric {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxInt = 2_147_483_647;
+		int minInt = -2_147_483_648;
+		
+		if (maxInt = Integer.MAX_VALUE) {
+			System.out.println("maxInt = "+ maxInt);
+		}
+		if (minInt = Integer.MIN_VALUE) {
+			System.out.println("minInt = "+ minInt);
+		}
+	}
+
+}
