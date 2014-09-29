2d2e705573d57e831ff4d307fbec51ea9be2a0c4
NurbekSakiev <sakin.snn@gmail.com>
2014-09-29 03:56:52 -0500

    the abstract class was added

---
+++ KodeJava/src/Animal.java
@@ -0,0 +1,19 @@
+
+public abstract class Animal {
+	private String species;
+	
+	public Animal (String species) {
+		this.species = species;
+	}
+	
+	public abstract void makeASound();
+	
+	public String getSpecies() {
+		return species;
+	}
+	
+	public static void main(String[] args){
+		Animal pig = new Pig("Warthog");
+		pig.makeASound();
+	}
+}
---
+++ KodeJava/src/Pig.java
@@ -0,0 +1,11 @@
+
+public class Pig extends Animal{
+
+	public Pig(String species) {
+		super(species);
+	}
+	
+	public void makeASound() {
+		System.out.print("oink oink");
+	}
+}
