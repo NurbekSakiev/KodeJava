a4e713ec03f04485bca86cd7e8034b211b01d6c5
NurbekSakiev <sakin.snn@gmail.com>
2014-09-29 02:06:03 -0500

    using of superclass constructor

---
+++ KodeJava/src/Female.java
@@ -0,0 +1,9 @@
+
+public class Female extends Human{
+	private String hairstyle;
+	
+	public Female (String hairstyle, String sex) {
+		super(sex);
+		this.hairstyle = hairstyle;
+	}
+}
---
+++ KodeJava/src/Human.java
@@ -0,0 +1,9 @@
+
+public class Human {
+	private String sex;
+	private int age;
+	
+	public Human(String sex) {
+		this.sex = sex;
+	}
+}
