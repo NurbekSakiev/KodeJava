4acebd526b3319ea4601f4bcfe0b09eb5ec7e90f
NurbekSakiev <sakin.snn@gmail.com>
2014-10-01 18:46:11 -0500

    changing double to int by using intValue() function

---
+++ KodeJava/src/DoubleToInt.java
@@ -0,0 +1,11 @@
+
+public class DoubleToInt {
+	public static void main(String[] args) {
+		Double number = 23.3;
+		System.out.println("The number is "+number);
+		
+		int number2 = number.intValue();
+		System.out.println("The int of double is "+number2);
+	}
+
+}
