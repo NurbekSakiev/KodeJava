87aa51b21fe3c10bdbf6a3a5a30a6b6cfaf37a85
NurbekSakiev <sakin.snn@gmail.com>
2014-10-05 23:33:39 -0500

    using ordinal() in enum

---
+++ KodeJava/src/EnumOrdinal.java
@@ -0,0 +1,14 @@
+enum Color {
+	RED,GREEN, BLUE
+}
+public class EnumOrdinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Color.RED :" +Color.RED.ordinal());
+		System.out.println("Color.BLUE :" +Color.BLUE.ordinal());
+		System.out.println("Color.GREEN :" +Color.GREEN.ordinal());
+		
+	}
+
+}
